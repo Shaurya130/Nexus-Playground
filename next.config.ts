eb3365e087301820a39be70312a95b4c674a0a27
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2025-12-19 20:13:48 +0530

    day 2

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
-  // Ensure build doesn't fail on missing env vars during static export
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
 };
 
 export default nextConfig;
